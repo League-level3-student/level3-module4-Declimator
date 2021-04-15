50b637768070284f3e9ca7437584b3cc49988e00
Declimator <31232768+Declimator@users.noreply.github.com>
2021-04-15 16:34:14 -0700

    TestMatchingBrackets

--- src/_01_TestMatchingBrackets/TestMatchingBrackets.java
+++ src/_01_TestMatchingBrackets/TestMatchingBrackets.java
@@ -3,30 +3,33 @@ package _01_TestMatchingBrackets;
 import java.util.Stack;
 
 public class TestMatchingBrackets {
-    /*
-     * Use a Stack to complete the method for checking if every opening bracket
-     * has a matching closing bracket
-     */
-    public static boolean doBracketsMatch(String b) {
-    	Stack<String> s = new Stack<String>();
-    	for(int i = 0; i < b.length(); i++) {
-    		s.add(b.charAt(i) + "");
-    	}
-    	int n = 0;
-    	while(!s.isEmpty()) {
+	/*
+	 * Use a Stack to complete the method for checking if every opening bracket has
+	 * a matching closing bracket
+	 */
+	public static boolean doBracketsMatch(String b) {
+		Stack<String> s = new Stack<String>();
+		for (int i = b.length()-1; i >=0; i--) {
+			s.add(Character.toString(b.charAt(i)));
+		}
+		int n = 0;
+		while (!s.isEmpty()) {
+			if (s.pop().equals("{")) {
+				n++;
+			} else {
+				n--;
+			}
+			if (n < 0) {
 
-    		if(s.peek() == "{") {
-    			s.pop();
-    			n++;
-    		}
-    		else {
-    			s.pop();
-    			n--;
-    		}
-    		if(n < 0) {
-    			return false;
-    		}
-    	}
-        return n==0;
-    }
+				return false;
+			}
+		}
+		if (n == 0) {
+
+			return true;
+		} else {
+
+			return false;
+		}
+	}
 }
--- src/_02_IntroToQueues/_01_IntroToQueue.java
+++ src/_02_IntroToQueues/_01_IntroToQueue.java
@@ -1,4 +1,9 @@
 package _02_IntroToQueues;
+
+import java.util.ArrayDeque;
+import java.util.Random;
+import java.util.Stack;
+
 /* 
  * OBJECTIVE:
  * 1. Push 100 double values onto a Stack.
@@ -31,25 +36,37 @@ package _02_IntroToQueues;
  */
 
 public class _01_IntroToQueue {
-    // 1. Create a Stack of Doubles using the Stack class
-    //    Note: you have to use the capitalized Double and not double
-    
-    // 2. Use a loop to add 100 random doubles between 0 and 100 to the Stack
-    
-    // 3. Create a Queue of Doubles using the ArrayDeque class
-    //    Note: you have to use the capitalized Double and not double
+	// 1. Create a Stack of Doubles using the Stack class
+	// Note: you have to use the capitalized Double and not double
+	// 2. Use a loop to add 100 random doubles between 0 and 100 to the Stack
+	public static void main(String[] args) {
+		Stack<Double> stack = new Stack<Double>();
+		int n;
+		for (int i = 0; i < 100; i++) {
+			stack.add(new Double(new Random().nextInt(100)));
+		}
 
-    // 4. Pop off 5 elements from the Stack and add them to the Queue 
+		// 3. Create a Queue of Doubles using the ArrayDeque class
+		// Note: you have to use the capitalized Double and not double
+		ArrayDeque<Double> q = new ArrayDeque<Double>();
+		// 4. Pop off 5 elements from the Stack and add them to the Queue
+		for (int i = 0; i < 5; i++) {
+			q.add(stack.pop());
+		}
+		// 5. Print and remove a random number of elements, from 1 to 5 elements,
+		// from the front of the Queue. Example:
+		// "removing 3 elements from Queue: 25 57 2"
+		n = new Random().nextInt(5) + 1;
+		System.out.print("removing " + n + " elements from Queue: ");
+		for (int i = 0; i < n; i++)
+			System.out.print(q.remove() + " ");
+		System.out.println("");
+		// 6. Pop off as many elements from the stack to fill the Queue with 5
+		// elements. If there aren't enough elements in the Stack to fill the
+		// queue, fill the queue as much as possible.
 
-    // 5. Print and remove a random number of elements, from 1 to 5 elements,
-    //    from the front of the Queue. Example:
-    //    "removing 3 elements from Queue: 25 57 2"
-    
-    // 6. Pop off as many elements from the stack to fill the Queue with 5
-    //    elements. If there aren't enough elements in the Stack to fill the 
-    //    queue, fill the queue as much as possible. 
-    
-    // 7. Loop until there are no more elements in either the Stack or Queue
-    //    and all the elements are printed
-    
+		// 7. Loop until there are no more elements in either the Stack or Queue
+		// and all the elements are printed
+	}
+
 }
